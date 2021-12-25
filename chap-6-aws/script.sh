02894599cb3f522262baf7c683546a9dfdaecb03
rayan <rayanbak257@gmail.com>
2021-12-25 14:12:59 +0100

    solving pass issue

--- chap-6-aws/script.sh
+++ chap-6-aws/script.sh
@@ -5,6 +5,5 @@ aws rds create-db-instance \
     --db-instance-class db.t3.micro \
     --engine mysql \
     --master-username admin \
-    --master-user-password admin99 \
+    --master-user-password adm1n9925pa55 \
     --allocated-storage 20
-done
